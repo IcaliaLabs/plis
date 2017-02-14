918a4aa9cfa60386b4a8c6537c31ed28f43d4a7f
Cejas <cejas@icalialabs.com>
2017-02-14 09:01:34 -0600

    Go best practices
    As a recommendation from the golang community, before any commit or submit,
    we should run `go fmt` to clean up our code.

--- plis.go
+++ plis.go
@@ -1,89 +1,89 @@
 package main
 
 import (
+	"github.com/IcaliaLabs/plis/cmd"
+	"github.com/urfave/cli"
 	"os"
-  "github.com/urfave/cli"
-  "github.com/IcaliaLabs/plis/cmd"
 )
 
 func main() {
-  app := cli.NewApp()
-  app.Name = "Plis"
-  app.Usage = "Translates common development actions into docker/docker-compose commands by asking nicely"
-  app.Version = "0.0.0.build6"
+	app := cli.NewApp()
+	app.Name = "Plis"
+	app.Usage = "Translates common development actions into docker/docker-compose commands by asking nicely"
+	app.Version = "0.0.0.build6"
 
-  app.Commands = []cli.Command{
-    {
-      Name:    "start",
-      Usage:   "Starts the project's containers",
-      Action:  cmd.Start,
-    },
-    {
-      Name:    "stop",
-      Usage:   "Stop the project's running processes",
-      Action:  cmd.Compose,
-      SkipFlagParsing: true,
-    },
-    {
-      Name:    "restart",
-      Usage:   "Restarts the project's running processes",
-      Action:  cmd.Compose,
-      SkipFlagParsing: true,
-    },
-    {
-      Name:    "attach",
-      Usage:   "Attach the console to a running process",
-      Action:  cmd.Attach,
-      SkipFlagParsing: true,
-    },
-    {
-      Name:    "run",
-      Usage:   "Runs a command in a running or new container of a particular service",
-      Action:  cmd.Run,
-      SkipFlagParsing: true,
-    },
-    {
-      Name:    "ps",
-      Usage:   "Lists the project's running processes",
-      Action:  cmd.Compose,
-      SkipFlagParsing: true,
-    },
-    {
-      Name:    "rm",
-      Usage:   "Removes the project's running processes",
-      Action:  cmd.Compose,
-      SkipFlagParsing: true,
-    },
-    {
-      Name:    "logs",
-      Usage:   "Opens the logs of running processes",
-      Action:  cmd.Compose,
-      SkipFlagParsing: true,
-    },
-    {
-      Name:    "down",
-      Usage:   "Stops and removes all containers",
-      Action:  cmd.Compose,
-      SkipFlagParsing: true,
-    },
-    {
-      Name:    "build",
-      Usage:   "Build or rebuild services",
-      Action:  cmd.Compose,
-      SkipFlagParsing: true,
-    },
-    {
-      Name:    "check",
-      Usage:   "Performs several checks inside your project",
-      Subcommands: []cli.Command{
-        {
-          Name:  "context",
-          Usage: "Displays the project context",
-          Action: cmd.CheckContext,
-        },
-      },
-    },
-  }
+	app.Commands = []cli.Command{
+		{
+			Name:   "start",
+			Usage:  "Starts the project's containers",
+			Action: cmd.Start,
+		},
+		{
+			Name:            "stop",
+			Usage:           "Stop the project's running processes",
+			Action:          cmd.Compose,
+			SkipFlagParsing: true,
+		},
+		{
+			Name:            "restart",
+			Usage:           "Restarts the project's running processes",
+			Action:          cmd.Compose,
+			SkipFlagParsing: true,
+		},
+		{
+			Name:            "attach",
+			Usage:           "Attach the console to a running process",
+			Action:          cmd.Attach,
+			SkipFlagParsing: true,
+		},
+		{
+			Name:            "run",
+			Usage:           "Runs a command in a running or new container of a particular service",
+			Action:          cmd.Run,
+			SkipFlagParsing: true,
+		},
+		{
+			Name:            "ps",
+			Usage:           "Lists the project's running processes",
+			Action:          cmd.Compose,
+			SkipFlagParsing: true,
+		},
+		{
+			Name:            "rm",
+			Usage:           "Removes the project's running processes",
+			Action:          cmd.Compose,
+			SkipFlagParsing: true,
+		},
+		{
+			Name:            "logs",
+			Usage:           "Opens the logs of running processes",
+			Action:          cmd.Compose,
+			SkipFlagParsing: true,
+		},
+		{
+			Name:            "down",
+			Usage:           "Stops and removes all containers",
+			Action:          cmd.Compose,
+			SkipFlagParsing: true,
+		},
+		{
+			Name:            "build",
+			Usage:           "Build or rebuild services",
+			Action:          cmd.Compose,
+			SkipFlagParsing: true,
+		},
+		{
+			Name:  "check",
+			Usage: "Performs several checks inside your project",
+			Subcommands: []cli.Command{
+				{
+					Name:   "context",
+					Usage:  "Displays the project context",
+					Action: cmd.CheckContext,
+				},
+			},
+		},
+	}
 
-  app.Run(os.Args)
+	app.Run(os.Args)
 }
