87dd830aef95ba40a804cf4705b141e9dc7b26cc
Roberto Quintanilla <roberto.quintanilla@gmail.com>
2017-07-21 13:24:32 -0500

    Relase of version 0.0.0.build10

--- plis.go
+++ plis.go
@@ -10,7 +10,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "Plis"
 	app.Usage = "Translates common development actions into docker/docker-compose commands by asking nicely"
-	app.Version = "0.0.0.build9"
+	app.Version = "0.0.0.build10"
 
 	app.Commands = []cli.Command{
 		{
