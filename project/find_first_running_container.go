ab8b30b62dc365e08279ac9d2ae418a2a3490e01
Roberto Quintanilla <roberto.quintanilla@gmail.com>
2018-12-22 17:34:27 -0600

    Fixed failure to match running containers when directory has dashes

--- project/find_first_running_container.go
+++ project/find_first_running_container.go
@@ -1,12 +1,23 @@
 package project
 
 import (
+	"log"
+	"os"
+	"path/filepath"
 	"regexp"
 )
 
+// FindFirstRunningContainer finds the state of the first running container
+// matching the given service name.
 func FindFirstRunningContainer(serviceName string, containers []ContainerState) ContainerState {
 	var foundContainer ContainerState
-	rp := regexp.MustCompile("^\\w+_" + serviceName + "_\\d+")
+	currentWorkdir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dirName := filepath.Base(currentWorkdir)
+	rp := regexp.MustCompile("^" + dirName + "_" + serviceName + "_\\d+")
 
 	for i := range containers {
 		if rp.FindString(containers[i].Name) != "" {
